1407cd37a3b7634848b2be9c67885a9a41dfec59
quarkend <39971072+quarkend@users.noreply.github.com>
2022-05-26 14:48:43 +0200

    UPDATE

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { AuthContext } from "./../../App";
+import "./login.css"
+export const Login = () => {
+    const { dispatch } = React.useContext(AuthContext);
+    const initialState = {
+        email: "",
+        password: "",
+        isSubmitting: false,
+        errorMessage: null
+    };
+    const [data, setData] = React.useState(initialState);
+    const handleInputChange = event => {
+        setData({
+            ...data,
+            [event.target.name]: event.target.value
+        });
+    };
+    const handleFormSubmit = event => {
+        event.preventDefault();
+        setData({
+            ...data,
+            isSubmitting: true,
+            errorMessage: null
+        });
+        fetch("/auth/login", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: data.email,
+                password: data.password
+            })
+        })
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                }
+                throw res;
+            })
+            .then(resJson => {
+                dispatch({
+                    type: "LOGIN",
+                    payload: resJson
+                })
+            })
+            .catch(error => {
+                setData({
+                    ...data,
+                    isSubmitting: false,
+                    errorMessage: error.message || error.statusText
+                });
+            });
+    };
+    return (
+        <div className="login-container">
+            <div className="cardlogin">
+                <div className="container">
+                    <form onSubmit={handleFormSubmit}>
+                        <h1>Login</h1>
+                        <label htmlFor="email">
+                            Email Address
+                            <input
+                                type="email"
+                                value={data.email}
+                                onChange={handleInputChange}
+                                name="email"
+                                id="email"
+                            />
+                        </label>
+                        <label htmlFor="password">
+                            Password
+                            <input
+                                type="password"
+                                value={data.password}
+                                onChange={handleInputChange}
+                                name="password"
+                                id="password"
+                            />
+                        </label>
+                        {data.errorMessage && (
+                            <span className="form-error">{data.errorMessage}</span>
+                        )}
+                        <button disabled={data.isSubmitting}>
+                            {data.isSubmitting ? (
+                                "Loading..."
+                            ) : (
+                                "Login"
+                            )}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Login;
